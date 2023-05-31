c5b595862598e5dc4965ef840fc7bd7bb2b597fc
Chris Eakins <ceakins92@gmail.com>
2023-05-31 11:13:56 -0600

    Nav update

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,12 +2,11 @@ import chaossymbol from "../static/img/chaossymbol.png"
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function CollapsibleExample() {
+export default function NavBar() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
+    <Navbar collapseOnSelect expand="lg" bg="black" variant="dark" sticky="top">
+      
         <Navbar.Brand><img src={chaossymbol} height="90" width="90"/></Navbar.Brand>
         <Navbar.Brand href="#home">Comic Chaos</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -26,9 +25,7 @@ function CollapsibleExample() {
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
-      </Container>
+      
     </Navbar>
   );
 }
-
-export default CollapsibleExample;
