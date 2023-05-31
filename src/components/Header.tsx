57b66dd910853f115d47c1e4ad2a4f5a56d1063a
Chris Eakins <ceakins92@gmail.com>
2023-05-31 11:03:35 -0600

    Update to Nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,12 +8,13 @@ const cardText = {
 
 export const Header = () => {
     return (
-        <Card className="bg-dark text-white">
+        <Card className="title.card bg-dark text-white">
             <Card.Img src={hero1} alt="Comic Chaos"/>
             <Card.ImgOverlay>
                 <Card.Title className="hero-title text-center mt-100"></Card.Title>
                 <Card.Text style={cardText} className="hero-text text-center">
-                    <br/><br/><br/><br/><br/><br/><br/>community | knowledge base | market
+                    <br/><br/><br/><br/><br/><br/><br/>
+                    community | knowledge base | market
                 </Card.Text>
             </Card.ImgOverlay>
         </Card>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,23 +1,34 @@
-import Navbar from "react-bootstrap/Navbar"
-import Nav from "react-bootstrap/Nav"
-import Container from "react-bootstrap/Container"
-import 'bootstrap/dist/css/bootstrap.min.css'
+import chaossymbol from "../static/img/chaossymbol.png"
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-export default function NavBarMain(){
-
-    return (
-        <>
-        <Navbar bg='dark' sticky='top' variant='dark' className='NavBar'>
-            <Container>
-                <Navbar.Brand>Comic Chaos</Navbar.Brand>
-                <Nav.Link href="#">Home</Nav.Link>
-                <Nav.Link href="#">About</Nav.Link>
-                <Nav.Link href="#">Browse</Nav.Link>
-                <Nav.Link href="#">Register</Nav.Link>
-                <Nav.Link href="#">Sign-In</Nav.Link>
-            </Container>
-        </Navbar>
-       
-        </>
-    );
+function CollapsibleExample() {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand><img src={chaossymbol} height="90" width="90"/></Navbar.Brand>
+        <Navbar.Brand href="#home">Comic Chaos</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">About</Nav.Link>
+            <Nav.Link href="#pricing">Browse Comics</Nav.Link>
+            <Nav.Link href="#pricing">Market</Nav.Link>
+            <Nav.Link href="#pricing">Community</Nav.Link>
+            <Nav.Link href="#pricing">CoD</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">Sign-In</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Search
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
+
+export default CollapsibleExample;
